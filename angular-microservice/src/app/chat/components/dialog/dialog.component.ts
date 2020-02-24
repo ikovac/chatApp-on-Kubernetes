c4d86b86d0fd2c7d6d96c71d338f4c68e3c79edd
Ivo <ikovacevic96k@gmail.com>
2020-02-24 18:11:39 +0100

    Angular - Styled both create group and create new conversation

--- angular-microservice/src/app/chat/components/dialog/dialog.component.ts
+++ angular-microservice/src/app/chat/components/dialog/dialog.component.ts
@@ -5,9 +5,10 @@ import { IUser } from 'src/app/shared/interfaces/iuser';
 import { Observable } from 'rxjs';
 
 export interface DialogData {
-  conversationParticipants: any;
-  isGroup: boolean;
-  groupName: string;
+  conversationParticipants?: IUser;
+  isGroup?: boolean;
+  groupName?: string;
+  groupParticipants?: number[];
 }
 
 
@@ -34,6 +35,41 @@ export class DialogComponent implements OnInit {
   }
 
   onFriendClick(friend) {
-    this.data.conversationParticipants = friend.id;
+    this.data.conversationParticipants = friend;
+  }
+
+  onFriendGroupClick(friend) {
+    if (!this.data.groupParticipants) {
+      this.data.groupParticipants = [];
+    }
+    if (!this.data.groupParticipants.includes(friend.id)) {
+      this.data.groupParticipants.push(friend.id);
+    } else {
+      this.data.groupParticipants = this.data.groupParticipants.filter(el => el !== friend.id);
+    }
+  }
+
+  isFriendSelectedInGroup(friendId) {
+    return (this.data.groupParticipants || []).includes(friendId);
+  }
+
+  isFriendSelectedInConversation(friendId) {
+    const { conversationParticipants } = this.data;
+
+    if (!conversationParticipants) {
+      return false;
+    }
+
+    return conversationParticipants.id === friendId;
+  }
+
+  checkIfValid() {
+    const { isGroup, groupParticipants, groupName, conversationParticipants } = this.data;
+
+    if (isGroup) {
+      return (groupParticipants && groupParticipants.length && groupName) ? true : false;
+    }
+
+    return conversationParticipants ? true : false;
   }
 }
--- angular-microservice/src/app/chat/components/message-buttons-section/message-buttons-section.component.ts
+++ angular-microservice/src/app/chat/components/message-buttons-section/message-buttons-section.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { DialogComponent } from '../dialog/dialog.component';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-message-buttons-section',
@@ -8,8 +9,6 @@ import { DialogComponent } from '../dialog/dialog.component';
   styleUrls: ['./message-buttons-section.component.scss']
 })
 export class MessageButtonsSectionComponent implements OnInit {
-  conversationParticipants: any;
-  groupName: string;
 
   constructor(public dialog: MatDialog) { }
 
@@ -19,14 +18,22 @@ export class MessageButtonsSectionComponent implements OnInit {
   openDialog(isGroup: boolean): void {
     const dialogRef = this.dialog.open(DialogComponent, {
       width: '400px',
-      data: {conversationParticipants: this.conversationParticipants, isGroup, groupName: this.groupName}
+      data: {
+        conversationParticipants: null,
+        isGroup,
+        groupName: null,
+        groupParticipants: [],
+      }
     });
 
-    /* dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      console.log("result: ", result);
-      this.animal = result;
-    }); */
+    dialogRef.afterClosed()
+      .pipe(takeWhile(result => result))
+      .subscribe(result => {
+        console.log("result: ", result);
+        // Send request to server depends on isGroup
+
+        // After server returns respons make selectedConversation = serverResponse + isGroup, conversationName
+      });
   }
 
 }
